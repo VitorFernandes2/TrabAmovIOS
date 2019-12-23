edd6fe1cf1cfc2a824b7323981fc73637e4653d5
isJoaoCoelho <55954421+isJoaoCoelho@users.noreply.github.com>
2019-12-23 02:15:41 -0800

    base trabalho

---
+++ ReceitasCulin/ReceitasCulin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceitasCulin
+//
+//  Created by test on 23/12/2019.
+//  Copyright © 2019 Isec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
